7ea056dccd20cd6721cccc400108aa210867ee80
Tom Benneche <tom@tjbenneche.com>
2014-03-10 21:16:41 -0500

    latest

--- mxl-build-test/games/js/games.js
+++ mxl-build-test/games/js/games.js
@@ -2,6 +2,40 @@
 $('.carousel').carousel()
 
 
+// When the page finishes loading
+window.onload = function () {
+
+	// Grab the email form
+	$("#contact-form").submit(function(event) {
+
+		// Create an AJAX request object
+		var api_request = new XMLHttpRequest();
+		api_request.open("POST", "http://mobilexlabs.com/feedback.php", true);
+		api_request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+		// Set the data to send. We want to grab the email input and the feedback input
+		api_request.send("email=" + $("#email-input").value + "&feedback="+$("#feedback-input"));
+
+		api_request.onreadystatechange = function() {
+
+			// When the request finishes...
+			if (api_request.readyState == 4) {
+
+				// Check if it is was successful
+				if (api_request.status != 200) {
+					alert("Could Not Send Feedback");
+				} else {
+					alert("Thanks for getting in touch!");
+				};
+			};
+		};
+		event.preventDefault();
+	});
+};
+
+
+
+
 $('#small-jamkings').on('click', function(){
 	event.preventDefault()
 	$('#big-clumsy-biker, #big-urban, #big-waka').hide();
